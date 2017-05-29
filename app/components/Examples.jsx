ad4a83c1fab78499692832f9d75312802f6f91fa
Kazim Hussain <kazimhussain685@gmail.com>
2017-05-29 16:09:14 +0500

    comment removed

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,11 +1,4 @@
 var React=require('react');
-// var Example=React.createClass({
-//     render : function(){
-//      return(
-//      <h3>Example Component</h3>
-//       );
-//     }
-// });
 
 var Example = (props) => {
      return(
