12bedcceaffb47ccc61584ab594bb63d1d0b8268
Kazim Hussain <kazimhussain685@gmail.com>
2017-05-31 14:02:55 +0500

    Prepartion for heroku

--- server.js
+++ server.js
@@ -3,10 +3,20 @@ var express=require('express');
 //Create app
 var app=express();
 
+const PORT = process.env.PORT || 3000;
+
+app.use(function(req,res,next){
+    if(req.headers['x-forwarded-proto'] === 'htpp'){
+       next();
+    }
+    else{
+     res.redirect('http://'+req.hostname+req.url);
+    }
+})
 //Folder to serve
 app.use(express.static('public'));
 
 //start server
-app.listen(3000,function(){
-   console.log('express server up on port 3000');
+app.listen(PORT,function(){
+   console.log('express server up on PORT ' + PORT);
 });
